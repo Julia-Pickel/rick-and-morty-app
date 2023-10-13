3fc602955520c63bb0999e20fef7d16db7b97db8
Julia Pickel <juliapickel@gmx.de>
2023-10-13 11:31:56 +0200

    task1 completed

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { createCharacterCard } from "./components/card/card.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -12,3 +14,5 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+createCharacterCard();
